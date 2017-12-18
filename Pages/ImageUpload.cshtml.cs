e84541a319da777e883eb91e961ab3602a34a748
Chilas <iamchilas@gmail.com>
2017-12-18 17:29:04 +0100

    chore: make file upload on post

--- Pages/ImageUpload.cshtml.cs
+++ Pages/ImageUpload.cshtml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,33 @@ namespace jqueryRazorpagesSimpleFileUpload.Pages
 {
     public class ImageUploadModel : PageModel
     {
-        public void OnGet()
-        {
 
+        //public IFormFile Image { get; set; }
+        public string RecentImagePath { get; set; }
+        public string ErrorMessage { get; set; }
+        public void OnGetAsync(string filename)
+        {
+            if (!string.IsNullOrEmpty(filename))
+            {
+                RecentImagePath = $"/uploads/{filename}";
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync(IFormFile image, string name)
+        {
+            if (image == null || image.Length == 0) return RedirectToPagePermanent("./ImageUpload");
+            var path = Path.Combine(
+                        Directory.GetCurrentDirectory(), "wwwroot/uploads",
+                        image.FileName);
+
+            var memory = new MemoryStream();
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await image.CopyToAsync(stream);
+            }
+
+            return RedirectToPagePermanent("./ImageUpload", new { filename = image.FileName });
         }
     }
 }
